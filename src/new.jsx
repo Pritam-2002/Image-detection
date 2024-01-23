3288bfe6fb600e06c97c7926f93e5b044e68274e
Pritam Mukherjee <91896082+Pritam-2002@users.noreply.github.com>
2024-01-23 12:00:51 +0530

    Update new.jsx

--- src/new.jsx
+++ src/new.jsx
@@ -95,16 +95,7 @@ const ImageRectangles = ({ imageUrl }) => {
   );
 };
 
-const App = () => {
-  // Replace 'your-image-url.jpg' with the actual image URL
-  const imageUrl = 'https://imgs.search.brave.com/Q4ot-TLikHZivRVu4QwWzomdZnyhOtpvtYtKy-w5uvc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdXBw/b3J0LmNvbnRlbnQu/b2ZmaWNlLm5ldC9l/bi11cy9tZWRpYS9k/NWNhNTkwYy0zNTE2/LTQzMWEtOTQwZC0z/ZWI3ZGUzODIyYzYu/cG5n';
 
-  return (
-    <div className="App">
-      <h1>Hello OpenCV in React</h1>
-      <ImageRectangles imageUrl={imageUrl} />
-    </div>
-  );
-};
 
-export default App;
+export default ImageRectangles;
+
